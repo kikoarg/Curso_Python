58ab096adc0ccd964281e3e5ae8cca501fbe7939
kikoarg <kiko_kpo@hotmail.com>
2022-06-15 19:32:33 -0300

    Test

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,11 +1,5 @@
 # Hola Mundo! [Python]
 # ---------------------------
-# Autor: Inove Coding School
-# Version: 2.0
-
-# Descripcion:
-# Programa creado para ensayar el correcto funcionamiento
-# del entorno de instalación Python
 import os
 
 name = input("Introduce tu nombre: ")
